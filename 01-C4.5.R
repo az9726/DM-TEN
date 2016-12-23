0bb28a00454b8730d175c46f7dfe679907a6487a
az9726 <az9726@126.com>
2016-12-23 09:20:18 +0800

    add input do not equal output

--- 01-C4.5.R
+++ 01-C4.5.R
@@ -82,4 +82,36 @@ plot(m1)
 # 
 # > plot(m1)
 # > 
-
+# 正式使用时会发现这个现象，输入的数据是100条，但是输出的数据结果不足一百条，用rpart包中的数据car.test.frame进行试验可以看到结果
+#> nrow(Train_Car);nrow(Test_Car)
+#[1] 45
+#[1] 15
+#> formula=Oil_Consumption~Price+Country+Reliability+Type+Weight+Disp.+HP
+#> C45_0=J48(formula,Train_Car)
+#> summary(C45_0)
+#
+#=== Summary ===
+#
+#Correctly Classified Instances          32               88.8889 %
+#Incorrectly Classified Instances         4               11.1111 %
+#Kappa statistic                          0.811 
+#Mean absolute error                      0.1093
+#Root mean squared error                  0.2338
+#Relative absolute error                 27.6786 %
+#Root relative squared error             52.8652 %
+#Total Number of Instances               36     
+#
+#=== Confusion Matrix ===
+#
+#  a  b  c   <-- classified as
+# 20  0  0 |  a = A
+#  1  6  3 |  b = B
+#  0  0  6 |  c = C
+#Train_Car中有45条数据，结果中只有“Total Number of Instances               36     ”，可能和“剪枝”有关。
+#结果的检验分析可以使用这个命令
+#> table(Train_Car$Oil_Consumption,predict(C45_0,Train_Car))
+#   
+#     A  B  C
+#  A 25  1  0
+#  B  1  8  3
+#  C  0  0  7
